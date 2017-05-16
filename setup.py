54ff159af19bda3d7b28c3f963812e6045697041
Javier <javialonso007@hotmail.es>
2017-05-16 10:57:18 +0200

    Included "setup.py" for installation
    Run "setup.py" in Command Line by typing: "python setup.py install"

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py-GSearch-API',
+    version='1.0',
+    packages=['gsearch'],
+    url='https://github.com/Javinator9889/py-GSearch-API',
+    license='GNU General Public License v3.0',
+    author='Javinator9889',
+    author_email='',
+    description='A simple Python API for searching web with Google'
+)

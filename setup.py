75862c300bf2516b5d65493896e0c5acd9fee798
Javier <javialonso007@hotmail.es>
2017-05-27 13:39:29 +0200

    Updated to version 1.3 (details in gsearch.py)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-GSearch-API',
-    version='1.2',
+    version='1.3',
     packages=['gsearch'],
     url='https://github.com/Javinator9889/py-GSearch-API',
     download_url='https://github.com/Javinator9889/py-GSearch-API/archive/master.zip',
